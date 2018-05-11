69693fa3dbbacb686af02ea3d1a8a801ea0b19c0
ezacca <ezacca@silentiumapps.com>
2018-05-11 15:00:28 -0300

    introducing state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { IAppState } from "../state/app.state";
+import { Store, select } from "@ngrx/store";
+import { INCREMENT, DECREMENT } from "../state/reducers/counter.reducer";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "app";
-  counter = 0;
+  counter: Observable<number>;
+
+  constructor(private store: Store<IAppState>) {}
+
+  ngOnInit() {
+    this.store.subscribe(state => {
+      console.log("-------------");
+      console.log("NEW STATE");
+      console.log(state);
+    });
+    this.counter = this.store.pipe(select(state => state.counter));
+  }
 
   incrementar() {
-    this.counter++;
+    /*console.log("--------------");
+    console.log("ACTION DISPATCHED");
+    console.log({ type: INCREMENT });*/
+    this.store.dispatch({ type: INCREMENT });
   }
   decrementar() {
-    this.counter--;
+    /*console.log("--------------");
+    console.log("ACTION DISPATCHED");
+    console.log({ type: DECREMENT });*/
+    this.store.dispatch({ type: DECREMENT });
   }
 }
