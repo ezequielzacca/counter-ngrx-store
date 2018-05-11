16e96e6403e765c6d4180a6e2fa0df4613be088f
ezacca <ezacca@silentiumapps.com>
2018-05-11 13:09:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  counter = 0;
+
+  incrementar() {
+    this.counter++;
+  }
+  decrementar() {
+    this.counter--;
+  }
 }
